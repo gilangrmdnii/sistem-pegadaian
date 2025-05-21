9535ef3177eeb5d052e032b7cd4164ea74abe22b
gilangrmdhni <gilangrmdhni1003@gmail.com>
2025-05-22 00:34:23 +0700

    remove management

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,6 +5,6 @@
 @section('content')
 <div class="bg-white shadow rounded p-6">
     <h2 class="text-2xl font-semibold mb-2">Selamat Datang 👋</h2>
-    <p class="text-gray-700">Halo {{ Auth::user()->name }}, kamu berhasil login ke sistem manajemen penggadaian.</p>
+    <p class="text-gray-700">Halo {{ Auth::user()->name }}, kamu berhasil login ke sistem  penggadaian.</p>
 </div>
 @endsection
